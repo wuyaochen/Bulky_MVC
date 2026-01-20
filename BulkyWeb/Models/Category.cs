02ea9e94ed571785f2eb40218d19230efe4c16b0
wyc <wyc@example.com>
2026-01-20 09:45:19 +0800

    NugetDownload

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    // 這個是用來建立資料庫的Row名稱
+    public class Category
+    {
+        [Key] // 表示這個是主鍵
+        public int Id { get; set; }
+        [Required] // 表示這個欄位是必填的
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+    }
+}
