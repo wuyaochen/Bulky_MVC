97066aad8ddf49502d34f2fbeac4d053e8afb2b6
wyc <wyc@example.com>
2026-01-20 11:58:22 +0800

    建立 Category 資料表及註冊 ApplicationDbContext 新增 ApplicationDbContext 並註冊至 DI，設定 SQL Server 連線字串。加入首次 migration，建立 MyProperty (Category) 資料表，包含 Id、Name、DisplayOrder 欄位。同步更新模型快照，並調整專案檔移除 Data 資料夾宣告。

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // 配置基本架構，並且繼承自 Entity Framework 的 DbContext
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> MyProperty { get; set; }
+        // 這個是用來對應 Category 這個 Model 的資料表，且會自動建立資料表
+    }
+}
