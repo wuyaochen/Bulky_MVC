18a408218cf98130d4ccf78ff164b96694f63ef9
wyc <wyc@gmail.com>
2026-02-08 09:00:34 +0800

    實作泛型 Repository 並公開 IRepository 介面
    將 IRepository<T> 介面由 internal 改為 public，並新增泛型 Repository<T> 類別，實作基本的新增、查詢、刪除等資料存取方法。補充必要的 using 指令以支援泛型查詢與 Entity Framework 操作。

---
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,52 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepositor;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet; // define T, so dbSet will become Category table in database
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>(); 
+            // _db.Categories == dbSet
+        }
+        public void Add(T entity)
+        {
+            _db.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+            // equal to Category.Controller (Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);)
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            _db.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            _db.RemoveRange(entity);
+        }
+    }
+}
