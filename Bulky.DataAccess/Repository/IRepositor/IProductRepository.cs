679c0c02bc7b8dd77f8550252e40c283ce4df4f1
wuyaochen <a1351015@gm.ncue.edu.tw>
2026-02-09 19:14:01 +0800

    新增產品(Product)CRUD功能與後台選單整合
    新增 Product 的 Controller 及 Razor View，實作完整 CRUD（新增、編輯、刪除、列表）功能。調整 IProductRepository 介面存取範圍，並於後台選單加入產品管理入口。修正 Create 頁面按鈕標籤錯誤。

--- Bulky.DataAccess/Repository/IRepositor/IProductRepository.cs
+++ Bulky.DataAccess/Repository/IRepositor/IProductRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Bulky.DataAccess.Repository.IRepositor
 {
-    internal interface IProductRepository : IRepository<Product>
+    public interface IProductRepository : IRepository<Product>
     {
         void Update(Product obj);
     }
