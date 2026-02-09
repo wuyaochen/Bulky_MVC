1225757ef29c92fb3b3df324ab6e8ac572094e1b
wuyaochen <a1351015@gm.ncue.edu.tw>
2026-02-09 18:32:35 +0800

    新增 Product 模型、資料表與 Repository 支援
    本次提交包含以下重點：
    - 新增 Product 模型，並加入資料驗證屬性
    - ApplicationDbContext 增加 Products 資料表及 6 筆種子資料
    - 新增資料庫遷移檔，建立 Products 資料表與種子資料
    - 新增 IProductRepository 介面與 ProductRepository 實作
    - UnitOfWork 整合 Product repository，支援資料存取與更新
    - 專案現已支援 Product CRUD 與資料存取層架構

---
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepositor;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    internal class ProductRepository : Repository<Product>, IProductRepository
+    {
+            private ApplicationDbContext _db;
+            // define Repository<Product> so that _db will become ApplicationDbContext
+            public ProductRepository(ApplicationDbContext db) : base(db)
+            {
+                // base(db) is for parent class Repository
+                // so that _db in Repository will become ApplicationDbContext
+                // and dbSet will become Product table in database
+                _db = db;
+            }
+    
+            public void Update(Product obj)
+            {
+                _db.Products.Update(obj);
+        }
+    }
+}
---
+++ Bulky.Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Models
+{
+    public class Product
+    {
+        [Key] // 表示這個是主鍵
+        public int Id { get; set; }
+        [Required] // 表示這個欄位是必填的
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [DisplayName("List Price")]
+        [Range(1, 10000, ErrorMessage = "List Price must be between 1 and 10000")]
+        public double ListPrice { get; set; }
+        [Required]
+        [DisplayName("Price for 1-50")]
+        [Range(1, 10000, ErrorMessage = "List Price must be between 1 and 10000")]
+        public double Price { get; set; }
+        [Required]
+        [DisplayName("Price for 50+")]
+        [Range(1, 10000, ErrorMessage = "List Price must be between 1 and 10000")]
+        public double Price50 { get; set; }
+        [Required]
+        [DisplayName("Price for 100+")]
+        [Range(1, 10000, ErrorMessage = "List Price must be between 1 and 10000")]
+        public double Price100 { get; set; }
+
+    }
+}
