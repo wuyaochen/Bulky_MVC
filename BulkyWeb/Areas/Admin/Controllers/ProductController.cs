0b64153e9148f832fd9ce281a8e01aeb5bdff9cd
wyc <wyc@example.com>
2026-02-10 11:51:16 +0800

    新增產品建立時的分類下拉選單
    在產品建立頁面加入分類選擇功能，於 Controller 取得分類資料並傳遞至 View，前端表單新增下拉選單綁定 CategoryId，提升使用者建立產品時的便利性與資料完整性。

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using Bulky.DataAccess.Repository.IRepositor;
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BulkyWeb.Areas.Admin.Controllers
 {
@@ -16,10 +17,18 @@ namespace BulkyWeb.Areas.Admin.Controllers
         public IActionResult Index()
         {
             List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+
             return View(objProductList);
         }
         public IActionResult Create()
         {
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            ViewBag.CategoryList = CategoryList;
+
             return View();
         }
         [HttpPost]
