4e39c1676b3f63835a44abd1d1af89c09fef6671
wyc <wyc@example.com>
2026-02-10 15:03:58 +0800

    重構Product建立頁面，改用ProductVM ViewModel
    將Product建立(Create)頁面與流程改為使用ProductVM ViewModel，讓Product資料與分類下拉選單(CategoryList)能同時傳遞至前端。調整Controller的Create方法與Create.cshtml表單欄位綁定，並於Product類別部分屬性加上[ValidateNever]。新增_ViewImports.cshtml引用與csproj必要參考，提升擴充性與維護性，符合ASP.NET Core MVC最佳實踐。

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository.IRepositor;
 using Bulky.Models;
+using Bulky.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -22,26 +23,39 @@ namespace BulkyWeb.Areas.Admin.Controllers
         }
         public IActionResult Create()
         {
-            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+            ProductVM productVM = new()
             {
-                Text = u.Name,
-                Value = u.Id.ToString()
-            });
-            // ViewBag.CategoryList = CategoryList;
-            ViewData["CategoryList"] = CategoryList;
-            return View();
+                Product = new Product(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                })
+            };
+            return View(productVM);
         }
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult Create(ProductVM productVM)
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj); //表單按下送出後，將資料加入資料庫
+                _unitOfWork.Product.Add(productVM.Product); //表單按下送出後，將資料加入資料庫
                 _unitOfWork.Save(); //save changes to database
                 TempData["success"] = "Product created successfully"; //記錄這個動作成功
                 return RedirectToAction("Index");
             }
-            return View();
+            else
+            {
+                //如果驗證失敗，重新載入CategoryList
+                productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+
+                return View(productVM);
+            }
+
         }
 
         public IActionResult Edit(int? id)
