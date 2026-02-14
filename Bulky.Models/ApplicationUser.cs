b979369ddc37fab1ecc52d8b17db4ba2c9d93584
wuyaochen <a1351015@gm.ncue.edu.tw>
2026-02-14 14:16:55 +0800

    擴充 IdentityUser 並新增 ApplicationUser 欄位
    新增 ApplicationUser 類別，繼承 IdentityUser，並加入 Name、StreetAddress、City、State、PostalCode 等欄位。更新 ApplicationDbContext 與資料庫遷移，讓 AspNetUsers 表支援 TPH 繼承與額外屬性。

---
+++ Bulky.Models/ApplicationUser.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Models
+{
+    // Inherit from IdentityUser to add additional properties if needed
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public int Name { get; set; }
+
+        public string? StreetAddress { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
+
+    }
+}
