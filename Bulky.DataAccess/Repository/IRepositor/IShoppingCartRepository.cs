d7566ce39275b63d2ae1cb7a628dfed2db047758
wyc <wyc@gmail.com>
2026-02-17 23:39:23 +0800

    新增購物車功能及資料存取層整合
    本次提交新增 ShoppingCart Model 與相關資料表遷移，並於 ApplicationDbContext 註冊 DbSet。實作 IShoppingCartRepository 及 ShoppingCartRepository，並整合至 UnitOfWork，完善購物車的資料存取層架構。此變更為後續購物車功能開發奠定基礎。

---
+++ Bulky.DataAccess/Repository/IRepositor/IShoppingCartRepository.cs
@@ -0,0 +1,16 @@
+﻿using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepositor
+{
+    public interface IShoppingCartRepository : IRepository<ShoppingCart>
+    {
+        void Update(ShoppingCart obj); 
+        // This method is specific to the Category repository.
+        // it may have additional logic for updating a category.
+    }
+}
---
+++ Bulky.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,30 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepositor;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private ApplicationDbContext _db;
+        // define Repository<ShoppingCart> so that _db will become ApplicationDbContext
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+        {
+            // base(db) is for parent class Repository
+            // so that _db in Repository will become ApplicationDbContext
+            // and dbSet will become ShoppingCart table in database
+            _db = db;
+        }
+
+        public void Update(ShoppingCart obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+        }
+    }
+}
