6ee1aa9559e24b58a88dfa9f2f2cd0ba066d8a1f
wyc <wyc@gmail.com>
2026-02-17 23:44:23 +0800

    新增 ApplicationUser Repository 與 UnitOfWork 整合
    新增 IApplicationUserRepository 介面與 ApplicationUserRepository 類別，並將其整合進 IUnitOfWork 與 UnitOfWork，統一管理 ApplicationUser 的資料存取，強化資料存取層的擴充性與一致性。

---
+++ Bulky.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepositor;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+        // define Repository<Category> so that _db will become ApplicationDbContext
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            // base(db) is for parent class Repository
+            // so that _db in Repository will become ApplicationDbContext
+            // and dbSet will become Category table in database
+            _db = db;
+        }
+    }
+}
