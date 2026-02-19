d0049582a67573f1faa3fe1c4ac8ea93be81150e
wuYaochen_acer <liyu11337@gmail.com>
2026-02-19 17:03:59 +0800

    購物車支援數量折扣與動態價格計算
    新增 ShoppingCart.Price 屬性（不映射資料庫），於 CartController 根據商品數量自動計算價格並累加訂單總金額。實作 GetPriceBaseOnQuantity 方法，支援不同數量區間的價格折扣。調整 using 與命名空間。

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 ﻿using Bulky.DataAccess.Repository.IRepositor;
+using Bulky.Models;
 using Bulky.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -27,8 +28,31 @@ namespace BulkyWeb.Areas.Customer.Controllers
                 ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
                 includeProperties: "Product")
             };
-
+            foreach (var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                cart.Price = GetPriceBaseOnQuantity(cart);
+                ShoppingCartVM.OrderTotal += cart.Price * cart.Count;
+            }
             return View(ShoppingCartVM);
+        }
+        private double GetPriceBaseOnQuantity(ShoppingCart shoppingCart)
+        {
+            if (shoppingCart.Count <= 50)
+            {
+                return shoppingCart.Product.Price;
+            }
+            else
+            {
+                if (shoppingCart.Count <= 100)
+                {
+                    return shoppingCart.Product.Price50;
+
+                }
+                else
+                {
+                    return shoppingCart.Product.Price100;
+                }
+            }
         }
     }
 }
