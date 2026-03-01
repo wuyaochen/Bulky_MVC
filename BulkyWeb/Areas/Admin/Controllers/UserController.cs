0b55449a955097fde7ba0854bff046d53922ea5b
wyc <wyc@gmail.com>
2026-03-01 19:04:31 +0800

    切換使用者鎖定狀態的 API 取代刪除功能
    將 UserController 的刪除方法改為 LockUnlock 方法，改用 [HttpPost] 並接收使用者 id，根據 LockoutEnd 狀態切換帳號鎖定或解鎖，並回傳操作結果 JSON，方便前端操作使用者鎖定狀態。

--- BulkyWeb/Areas/Admin/Controllers/UserController.cs
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -47,9 +47,27 @@ namespace BulkyWeb.Areas.Admin.Controllers
             return Json(new { data = objUserList });
         }
 
-        [HttpDelete]
-        public IActionResult Delete(int? id)
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody]string id)
         {
+            var objFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking" });
+            }
+
+            if (objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+            {
+                // user is currently locked, we need to unlock them
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                // user is currently unlocked, we will lock them
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(100);
+            }
+            _db.SaveChanges();
+
             return Json(new { success = true, message = "Delete successful" });
         }
         #endregion
